3cfb4b23000b5df8407b12b1742676e176d3e81f
Sarah Ahmad <saa804@nyu.edu>
2024-04-04 12:44:55 -0400

    adding comments to code

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//this function ensures that when showFrogImage is deployed, it recalls the yogi bear image and hides the yogi berra image
+
 function showFrogImage() {
     var imageUrl = "https://upload.wikimedia.org/wikipedia/en/thumb/f/f0/Yogi_Bear_Yogi_Bear.png/220px-Yogi_Bear_Yogi_Bear.png";
     document.getElementById("myImage1").src = imageUrl;
@@ -8,6 +10,7 @@ function hideFrogImage() {
     document.getElementById("imageContainer1").style.display = "none";
 }
 
+//this function ensures that when showKermitImage is deployed, it shows the yogi berra image and hides the yogi bear image
 function showKermitImage() {
     var imageUrl = "https://historicmissourians.shsmo.org/wp-content/uploads/2020/07/berra1-1.jpg";
     document.getElementById("myImage2").src = imageUrl;
